caba81ec978393f65c01d768061a931eabfdc3d8
Thomas van de Meer <t.vandemeer@gmail.com>
2024-02-25 11:09:25 +0100

    Alles, behalve bin folder, aan repo toegevoegd

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(IPokemonRepository pokemonRepository, IMapper mapper)
+        {
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+    }
+}
